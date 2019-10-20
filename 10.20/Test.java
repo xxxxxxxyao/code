2901ab20fc00e75bd0e31e368657d101437cf570
xxxxxxxyao <2274828126@qq.com>
2019-10-20 23:40:34 +0800

    10.20数组代码

---
+++ 10.20/Test.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+public class Test {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4}, {2, 3, 6, 7}, {3, 4, 6, 7}};
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
+   /* public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        String newArr = Arrays.toString(arr);
+        System.out.println(newArr);
+    }*/
+
+   /* public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+         int[] show=transform(arr);
+        printArray(show);
+    }
+
+    public static int[]  transform(int[] arr){
+        int[] ret=new int[arr.length];
+        for (int i = 0; i <arr.length ; i++) {
+            ret[i]=arr[i]*2;
+        }
+        return ret;
+    }*/
+
+   /* public static void transform(int[] arr){
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i]=arr[i]*2;
+        }
+    }*/
+
+  /*  public static void main(String[] args) {
+        int[] arr={1,2,3};
+        func(arr);
+        System.out.println("arr[0]="+arr[0]);
+    }
+
+    public static void func(int[] arr1){
+        arr1[0]=10;
+        System.out.println("arr1[0]"+arr1[0]);
+    }*/
+
+
+   /* public static void main(String[] args) {
+        int num = 0;
+        func(num);
+        System.out.println("num=" + num);
+    }
+
+    public static void func(int x){
+        x=10;
+        System.out.println("x="+x);
+    }*/
+
+    /*public static void printArray(int[] arr) {
+        for (int x : arr
+                ) {
+            System.out.println(x);
+        }
+    }
+}*/
+
+    /*public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        printArray(arr);
+    }*/
+
+       /*int[] arr={1,2,3,4,5};
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+        }*/
+
+
