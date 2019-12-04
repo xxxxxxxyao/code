cad1037b066392b4a080a92a64898eb1dfc9b3ca
xxxxxxxyao <2274828126@qq.com>
2019-12-04 16:51:59 +0800

    12.04字符串操作

---
+++ 20191204/src/Test.java
@@ -0,0 +1,67 @@
+public class Test {
+    public static void main(String[] args) {
+        String str="helloword";
+        System.out.println(str.substring(5));
+        System.out.println(str.substring(0,5));
+    }
+}
+
+   /* public static void main(String[] args) {
+        String str = "192.168.1.2";
+        String[] result = str.split("\\.");
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}*/
+   /* public static void main(String[] args) {
+        String str="hello word hello";
+        String[] result=str.split(" ",2);
+        for(String s:result){
+            System.out.println(s);
+        }
+    }*/
+    /*public static void main(String[] args) {
+        String str="imastudent";
+        System.out.println(str.replaceAll("t","--"));
+        System.out.println(str.replaceFirst("t","--"));
+    }*/
+
+   /* public static void main(String[] args) {
+        String str="helloword";
+        System.out.println(str.startsWith("w"));
+        System.out.println(str.startsWith("h"));
+        System.out.println(str.startsWith("o",2));
+        System.out.println(str.startsWith("o",4));
+        System.out.println(str.endsWith("rd"));
+    }*/
+    /*public static void main(String[] args) {
+        String str="youareagirl";
+        System.out.println(str.lastIndexOf("a"));
+    }*/
+    /*public static void main(String[] args) {
+        String str="howareyou";
+        System.out.println(str.indexOf("you",2));
+        System.out.println(str.indexOf("you",7));
+    }*/
+   /* public static void main(String[] args) {
+        String str="helloword";
+        System.out.println(str.indexOf("bit"));
+        System.out.println(str.indexOf("word"));
+
+    }*/
+   /* public static void main(String[] args) {
+        String str="helloword";
+        System.out.println(str.contains("word"));
+    }*/
+    /*public static void main(String[] args) {
+        System.out.println("a".compareTo("A"));
+        System.out.println("a".compareTo("A"));
+        System.out.println("A".compareTo("A"));
+    }*/
+   /* public static void main(String[] args) {
+        String a = "abcde";
+        String b = "aBcde";
+        System.out.println(a.equalsIgnoreCase(b));
+    }*/
+
