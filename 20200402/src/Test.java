32bf32d92f01a060a28829cd21bfae45c3b63dfc
xxxxxxxyao <2274828126@qq.com>
2020-04-02 21:39:44 +0800

    0402day3

---
+++ 20200402/src/Test.java
@@ -0,0 +1,20 @@
+/*
+import java.util.*;
+
+public class Test{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String ss=sc.nextLine();
+
+        StringBuilder s1=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!ss.contains(s.substring(i,i+1))){
+                s1.append(c);
+            }
+        }
+
+        System.out.println(s1.toString());
+    }
+}*/
